4692b66e3f149bddf8d371cfb7a92d48a09294fc
Dominique Fober <fober@grame.fr>
2022-01-28 10:28:13 +0100

    in progress

---
+++ public/lib/comm.js
@@ -0,0 +1,53 @@
+
+var ws = null;
+var gNetMaxLatency = 300;
+
+function connect (url) {
+    if (!url)
+        url = location.origin.replace(/^http/, 'ws');
+    ws = new WebSocket(url);
+    if (ws) {
+        ws.onmessage = function(msg) { receive (msg.data); };
+        console.log ("Connected to", url)
+        // setInterval(() => { ws.send ('dummy'); }, 20000);
+    }
+    else console.log ("Failed to connect to", url);
+}
+
+function receive (data) {
+    console.log ("Receive", data);
+    const parts = data.split(' ');
+    switch (parts[0]) {
+        case 'DROP': 
+            playAt (parts[1]);
+            break;
+    }
+}
+
+function drop () { 
+    let date = gTime.now() + gNetMaxLatency;
+    ws.send ('DROP ' + date); 
+}
+
+function getDelay(date) {
+    let n = date - gTime.now();
+    if (n < 0) console.log("late event detected: delay", n)
+    return (n > 0) ? n : 0;
+}
+
+function playAt (date) {
+    let delay = getDelay(date);
+    let msg = inscore.newMessage();
+    inscore.msgAddF (msg, 1);
+    setTimeout( () => { inscore.postMessage("/ITL/scene/faust/faust/drop", msg);}, delay);
+    let off = inscore.newMessage();
+    inscore.msgAddF (off, 0);
+    setTimeout( () => { inscore.postMessage("/ITL/scene/faust/faust/drop", off);}, delay + 50);
+}
+
+var gTime = timesync.create({
+    server: '/timesync',
+    interval: 10000
+});
+if (!gTime) console.log ("Failed to create timesync");
+connect();
